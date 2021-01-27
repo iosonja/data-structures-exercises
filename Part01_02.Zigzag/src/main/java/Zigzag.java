da53500767753ce4b119b4d96fa178b6d5203852
seekaboo <40118812+seekaboo@users.noreply.github.com>
2021-01-27 09:58:07 +0200

    Working version of Sequence added

--- Part01_02.Zigzag/src/main/java/Zigzag.java
+++ Part01_02.Zigzag/src/main/java/Zigzag.java
@@ -35,11 +35,11 @@ public class Zigzag {
 
     public static void main(String[] args) {
         Zigzag z = new Zigzag();
-        //System.out.println(Arrays.toString(z.create(1))); // [1]
+        System.out.println(Arrays.toString(z.create(1))); // [1]
         System.out.println(Arrays.toString(z.create(2))); // [1,2]
-        //System.out.println(Arrays.toString(z.create(3))); // [3,1,2]
-        //System.out.println(Arrays.toString(z.create(4))); // [3,1,2,4]
-        //System.out.println(Arrays.toString(z.create(5))); // [5,3,1,2,4]
+        System.out.println(Arrays.toString(z.create(3))); // [3,1,2]
+        System.out.println(Arrays.toString(z.create(4))); // [3,1,2,4]
+        System.out.println(Arrays.toString(z.create(5))); // [5,3,1,2,4]
     }
 }
 
--- Part01_03.Sequence/src/main/java/Sequence.java
+++ Part01_03.Sequence/src/main/java/Sequence.java
@@ -10,8 +10,43 @@
  */
 public class Sequence {
     
+    public String calculate(String x) {
+        
+        int c = 1;
+        String previous = "";
+        String midvalue = "";
+        
+        for (int i = x.length(); i > 0; i--) {
+            
+            String current = x.substring(i-1, i);
+            
+            if (current.equals(previous)) {
+                c++;
+            } else if (previous != "" && !previous.equals(current)) {
+                midvalue = String.valueOf(c) + previous + midvalue;
+                c = 1;
+            } else if (i == 0) {
+                midvalue = String.valueOf(c) + current + midvalue;
+            }
+            
+            previous = current;
+        }
+        String value = String.valueOf(c) + previous + midvalue;
+
+        return value;
+    }
+    
     public String generate(int n) {
-        // TODO
+        String s = "1";
+        if (n == 1) {
+            return "1";
+        }
+        for (int i = 1; i < n; i++) {
+            s = calculate(s);
+        }
+        
+        return s;
+        
     }
 
     public static void main(String[] args) {
