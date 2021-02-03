f4700798bd2249798d684302d1822bce9e8d1e3f
seekaboo <40118812+seekaboo@users.noreply.github.com>
2021-02-03 09:12:55 +0200

    Hopefully final version of SplitList

--- Part02_03.SplitList/src/main/java/SplitList.java
+++ Part02_03.SplitList/src/main/java/SplitList.java
@@ -1,13 +1,52 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author sonjaek
  */
 public class SplitList {
     
+    public int count(int[] t) {
+        int sum = 0;
+        int size = t.length;
+        
+        int[] leftSide = new int[size];
+        int[] rightSide = new int[size];
+        
+        leftSide[0] = t[0];
+        rightSide[size - 1] = t[size];
+        
+        for (int i = 1; i < size; i++) {
+            if (leftSide[i - 1] > t[i]) {
+                leftSide[i] = leftSide[i - 1];
+            } else {
+                leftSide[i] = t[i];
+            }
+        }
+        
+        for (int i = size - 2; i >= 0; i--) {
+            if (rightSide[i + 1] < t[i]) {
+                rightSide[i] = rightSide[i + 1];
+            } else {
+                rightSide[i] = t[i];
+            }
+        }
+        
+        for (int i = 0; i < size - 1; i++) {
+            if (leftSide[i] < rightSide[i + 1]) {
+                if (leftSide[i] < leftSide[i + 1]) {
+                    if (rightSide[i] < rightSide[i + 1]) {
+                        sum++;
+                    }
+                }
+            }
+        }
+        
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        SplitList s = new SplitList();
+        System.out.println(s.count(new int[] {1,2,3,4,5})); // 4
+        System.out.println(s.count(new int[] {5,4,3,2,1})); // 0
+        System.out.println(s.count(new int[] {2,1,2,5,7,6,9})); // 3
+    }
 }
