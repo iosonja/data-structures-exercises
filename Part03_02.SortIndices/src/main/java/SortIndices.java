8d45c827b53c5de0054c60fabb1c47389b419094
Sonja Ek <sonja3k@icloud.com>
2021-02-09 17:11:44 +0200

    Add comments to SortIndices

--- Part03_02.SortIndices/src/main/java/SortIndices.java
+++ Part03_02.SortIndices/src/main/java/SortIndices.java
@@ -8,12 +8,17 @@ import java.util.stream.IntStream;
 public class SortIndices {
 
     public int[] get(int[] t) {
+        // Create array that will contain the indices to be returned:
         int[] indices = new int[t.length];
+        // Create copy of the original array and sort it:
         int[] sortedArray = Arrays.copyOf(t, t.length);
         Arrays.sort(sortedArray);
         
+        // Loop over the sorted array's values:
         for (int i = 0; i < t.length; i++) {
             final int currentValue = sortedArray[i];
+            
+            // Add the original index of the current value to indices array:
             indices[i] = IntStream.range(0, t.length)
                         .filter(j -> t[j] == currentValue)
                         .findFirst()
