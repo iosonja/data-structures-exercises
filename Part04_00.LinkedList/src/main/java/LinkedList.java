184292c9b7ef8eaad0e43e256820b9bbfdcfeb9e
Sonja Ek <sonja3k@icloud.com>
2021-02-15 18:41:21 +0200

    Add code to LinkedList

--- Part04_00.LinkedList/src/main/java/LinkedList.java
+++ Part04_00.LinkedList/src/main/java/LinkedList.java
@@ -1,13 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author sonjaek
  */
 public class LinkedList {
+
     
+    public static void main(String[] args) {
+        LinkedList b = new LinkedList();
+    }
 }
