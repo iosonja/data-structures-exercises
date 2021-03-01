fd18484fda6988c7a20e7534ffae24b8da3a8c47
Sonja Ek <sonja3k@icloud.com>
2021-03-01 14:28:30 +0200

    Complete a working version of Leaves

--- Part06_01.Leaves/src/main/java/Leaves.java
+++ Part06_01.Leaves/src/main/java/Leaves.java
@@ -1,10 +1,19 @@
 /**
- *
+ * The program counts how many leaves there are in a given binary tree
+ * Maximum number of nodes: 100
  * @author sonjaek
  */
 public class Leaves {
+        
     public int count(Node node) {
-        // TODO
+        if (node == null) {
+            return 0;
+        }
+        if (node.left == null && node.right == null) {
+            return 1;
+        } else {
+            return count(node.left) + count(node.right);
+        }
     }
 
     public static void main(String[] args) {
