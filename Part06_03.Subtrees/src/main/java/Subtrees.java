6bb8fad116103602ad0dda69ced77491402f1c53
Sonja Ek <sonja3k@icloud.com>
2021-03-01 17:13:55 +0200

    Add a version of Subtrees that currently doesn't work correctly

--- Part06_03.Subtrees/src/main/java/Subtrees.java
+++ Part06_03.Subtrees/src/main/java/Subtrees.java
@@ -3,8 +3,23 @@
  * @author sonjaek
  */
 public class Subtrees {
+    
+    public int count(Node node) {
+        if (node == null)
+            return 0;
+        if (node.right == null)
+            return count(node.left);
+        return 1 + count(node.left) + count(node.right);
+    }
+    
     public int diff(Node node) {
-        // TODO
+        if (node == null) {
+            return 0;
+        }
+        int left = count(node.left);
+        int right = count(node.right);
+        
+        return Math.abs(left - right);
     }
 
     public static void main(String[] args) {
