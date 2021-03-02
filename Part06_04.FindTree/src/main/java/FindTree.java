7607bbbebe803fc9d6442612dcc8be4fd685b9d5
Sonja Ek <sonja3k@icloud.com>
2021-03-02 10:32:07 +0200

    Add working algorithm for FindTree

--- Part06_04.FindTree/src/main/java/FindTree.java
+++ Part06_04.FindTree/src/main/java/FindTree.java
@@ -1,13 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- *
+ * This program counts how many times mainTree is included in another binary 
+ * tree as a subtree.
  * @author sonjaek
  */
 public class FindTree {
+    public boolean isSame(Node mainTreeNode, Node comparableTreeNode) {
+        if (mainTreeNode == null && comparableTreeNode == null) {
+            return true;
+        } else if ((mainTreeNode != null) && (comparableTreeNode != null)) {
+            return isSame(mainTreeNode.left, comparableTreeNode.left) && 
+                   isSame(mainTreeNode.right, comparableTreeNode.right);
+        }
+        return false;
+    }
     
+    public int count(Node mainTree, Node treeThatContainsMainTrees) {
+        if (treeThatContainsMainTrees == null) {
+            return 0;
+        }
+        int oneOrZero;
+        if (isSame(mainTree, treeThatContainsMainTrees)) {
+            oneOrZero = 1;
+        } else {
+            oneOrZero = 0;
+        }
+        int occurrences = oneOrZero
+                        + count(mainTree, treeThatContainsMainTrees.left)
+                        + count(mainTree, treeThatContainsMainTrees.right);
+        return occurrences;
+    }
+
+    public static void main(String[] args) {
+        FindTree f = new FindTree();
+        Node tree1 = new Node(new Node(null,null),
+                              new Node(null,null));
+        Node tree2 = new Node(null,
+                              new Node(new Node(null,null),
+                                       new Node(null,null)));
+        System.out.println(f.count(tree1,tree2)); // 1
+    }
 }
--- Part06_04.FindTree/src/main/java/Node.java
+++ Part06_04.FindTree/src/main/java/Node.java
@@ -1,13 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author sonjaek
  */
 public class Node {
-    
+    Node left, right;
+
+    public Node(Node left, Node right) {
+        this.left = left;
+        this.right = right;
+    }
 }
